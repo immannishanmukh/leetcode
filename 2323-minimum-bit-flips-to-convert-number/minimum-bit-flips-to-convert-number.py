a87ca61f8b61a9bdd9c118282cac01b36863e2ca
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-01-18 22:48:43 +0530

    Time: 17 ms (42.70%) | Memory: 13.2 MB (59.55%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,13 +1,20 @@
 class Solution(object):
-    def minBitFlips(self, start, goal):
+    def minBitFlips(self, a,b):
         """
         :type start: int
         :type goal: int
         :rtype: int
         """
         c=0
-        n=start^goal
-        while (n!=0) :
-            if (n&1==1): c+=1
+        # n=start^goal
+        # while (n!=0) :
+        #     if (n&1==1): c+=1
+        #     n=n>>1
+        # return c
+        n=max(a,b);
+        while (n!=0):
+            if (a&1!=b&1): c+=1
+            a=a>>1
+            b=b>>1
             n=n>>1
         return c
