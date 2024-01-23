1e7baef826f56e88997d3305669999caa8e91858
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-01-23 17:18:19 +0530

    Time: 8 ms (56.24%) | Memory: 10.6 MB (18.44%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        int x=INT_MAX;
-        ListNode* temp= head;
-        if (temp == NULL or temp -> next == NULL  or temp -> next -> next == NULL) return false;
-        while (temp) {
-            if (temp->val != x) {
-                temp->val = x;
-            }
-            else {
+        // int x=INT_MAX;
+        // ListNode* temp= head;
+        // if (temp == NULL or temp -> next == NULL  or temp -> next -> next == NULL) return false;
+        // while (temp) {
+        //     if (temp->val != x) {
+        //         temp->val = x;
+        //     }
+        //     else {
+        //         return true;
+        //     }
+        //     temp = temp -> next;
+        // }
+        // return false;
+        ListNode* rabbit = head;
+        ListNode* turtle = head;
+        while (rabbit != NULL and rabbit->next != NULL) {
+            rabbit = rabbit -> next -> next;
+            turtle = turtle -> next;
+            if (turtle == rabbit and rabbit !=NULL) {
                 return true;
             }
-            temp = temp -> next;
         }
         return false;
     }
