7ea2c3dd87dcdcba0565fc72bb9753b1acf8e427
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-01-27 22:41:04 +0530

    Time: 74 ms (95.25%) | Memory: 16.6 MB (56.03%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
@@ -1,3 +1,21 @@
+# class Solution:
+#     def maximumStrongPairXor(self, nums: List[int]) -> int:
+#         return max(x ^ y for x in nums for y in nums if abs(x - y) <= min(x, y))
+
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
-        return max(x ^ y for x in nums for y in nums if abs(x - y) <= min(x, y))
+        nums.sort()
+        start = end = mx = 0
+        while end < len(nums):
+            if abs(nums[end] - nums[start]) > min(nums[end], nums[start]):
+                start += 1
+                continue
+            for i in range(start, end):
+                mx = max(mx, nums[i] ^ nums[end])
+
+            end += 1
+        return mx
+
+
+
+        
