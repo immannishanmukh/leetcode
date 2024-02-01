43586556fca5191b05737474337834fe51898f81
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-02-01 21:23:13 +0530

    Time: 14 ms (17.82%) | Memory: 7.2 MB (7.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    // int fun(int a) {
-    //     if (a==0 ) {
-    //         return 1;
-    //     }
-    //     return fun(a-1)+fun(a-2);
-    // }
-    int fun(int i,int a, int b) {
-        if (i==0) return a;
-
-        return fun(--i,b,a+b);
+    int fun(int a) {
+        if (a<=1 ) {
+            return a;
+        }
+        return fun(a-1)+fun(a-2);
     }
+    // int fun(int i,int a, int b) {
+    //     if (i==0) return a;
+
+    //     return fun(--i,b,a+b);
+    // }
     int fib(int n) {
         if (n==0) return 0;
         if (n==1) return 1;
 
-        int  x  = fun(n,0,1);
-        //cout << x;
+        // int  x  = fun(n,0,1);
+        int  x  = fun(n);
+        cout << x;
+        // return x;
         return x;
     }
 };
