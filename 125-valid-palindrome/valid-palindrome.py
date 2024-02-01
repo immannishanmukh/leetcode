9b26fbf7a3bb874f810a79ac1cdbda56c411f957
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-02-01 17:34:52 +0530

    Time: 6 ms (99.98%) | Memory: 14.4 MB (58.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,10 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        a=""
-        for i in s:
-            if i in "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789":
-                a=a+i
-        a=a.lower()
-        if (a==a[::-1]): return True
-        else: return False
+        s=s.lower()
+        pattern =r'[^a-z0-9]+'
+        a = re.sub(pattern,'',s)
+        
+        if(a[::-1]==a):
+            return True
+        else:
+            return False
+        
