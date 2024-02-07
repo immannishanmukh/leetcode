be724f10d24b78b02d0673080fd5593c6633846f
immannishanmukh <119723008+immannishanmukh@users.noreply.github.com>
2024-02-07 18:22:09 +0530

    Time: 26 ms (82.05%) | Memory: 14.1 MB (91.03%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,14 +1,22 @@
-
 class Solution(object):
     def frequencySort(self, s):
-        char_freq = Counter(s)
-        print(char_freq)
-        max_heap = [(-freq, char) for char, freq in char_freq.items()]
-        print (max_heap)
-        heapq.heapify(max_heap)
-        result = ""
-        while max_heap:
-            freq, char = heapq.heappop(max_heap)
-            result += char * (-freq)
-            print (result)
-        return result
+        """
+        :type s: str
+        :rtype: str
+        """
+        checked=set()
+        letters=[]
+        count=[]
+        for letter in s:
+            if letter not in checked:
+                letters.append(letter)
+                count+=[s.count(letter)]
+                checked.add(letter)
+        word=''
+        length=len(count)
+        for i in range(length):
+            longest=max(count)
+            index=count.index(longest)
+            word=word+letters[index]*longest
+            count[index]=0
+        return word
